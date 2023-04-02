1c4d2e2f4445472ee3de2c796fba1c651f09a741
ciel2142 <igi2131@gmail.com>
2023-04-02 20:17:41 +0300

    t

--- module1/test.go
+++ module1/test.go
@@ -1,9 +1,29 @@
-package module1
+package main
+
+import "fmt"
 
 type TestType1 struct {
 	Name string
 }
 
+type TestType2 struct {
+	Name string
+}
+
 func (t *TestType1) HelloWorld() string {
 	return "Hello World " + t.Name
+}
+
+func (t TestType2) HelloWorld() string {
+	return "Hello World " + t.Name
+}
+
+func HelloWorld[T TestType1 | TestType2](s T) string {
+	return fmt.Sprintf("%+v", s)
+}
+
+func main() {
+	HelloWorld(TestType2{
+		"test",
+	})
 }
--- module2/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Ciel2142/gotest/awesomeProject4/module2
-
-go 1.20
